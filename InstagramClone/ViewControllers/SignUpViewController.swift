aa130008796c37fccc567b34ec3347656129655b
Martinz Chukz <martins.twist@gmail.com>
2020-02-14 13:12:58 +0100

    added firebase

--- InstagramClone/ViewControllers/SignUpViewController.swift
+++ InstagramClone/ViewControllers/SignUpViewController.swift
@@ -1,5 +1,8 @@
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
 
 class SignUpViewController: UIViewController {
     
@@ -8,15 +11,27 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    var selectedImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         profileImage.layer.cornerRadius = 40
         profileImage.clipsToBounds = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(SignUpViewController.handleSelectedProfileImage))
+        profileImage.addGestureRecognizer(tapGesture)
+        profileImage.isUserInteractionEnabled = true
         
         decorateTextField(textField: usernameTextField)
         decorateTextField(textField: emailTextField)
         decorateTextField(textField: passwordTextField)
+    }
+    
+    @objc func handleSelectedProfileImage() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.isEditing = true
+        present(imagePicker, animated: true, completion: nil)
     }
 
     @IBAction func dismiss_onClick(_ sender: Any) {
@@ -34,4 +49,46 @@ class SignUpViewController: UIViewController {
         textField.layer.addSublayer(bottomLayer)
     }
     
+    @IBAction func signUpBtn_tapped(_ sender: Any) {
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (result, error) in
+            if error != nil{
+                print(error!.localizedDescription)
+                return
+            }
+            let uid = result!.user.uid
+
+            let storageRef = Storage.storage().reference().child("users_profile").child(uid)
+            if let img = self.selectedImage, let data = img.jpegData(compressionQuality: 0.1){
+                storageRef.putData(data, metadata: nil, completion: { (metaData, error) in
+                    if error != nil{
+                        print("error saving image \( error!.localizedDescription)")
+                        return
+                    }
+                    storageRef.downloadURL(completion: { (url, error) in
+                        if error != nil{
+                            print("error get image url \(error!.localizedDescription)")
+                            return
+                        }
+                        let ref = Database.database().reference()
+                        let userReference = ref.child("user")
+                        let newUserRefernce = userReference.child(uid)
+                        newUserRefernce.setValue(["username": self.usernameTextField.text!, "email": self.emailTextField.text!, "profileImageUrl": url!.absoluteString])
+                    })
+                })
+            }else{
+                print("failed to pick image")
+            }
+    
+        }
+    }
+}
+
+extension SignUpViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let img  = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+           profileImage.image = img
+            selectedImage = img
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'InstagramClone' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for InstagramClone
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
+pod 'Firebase/Firestore'
+pod 'Firebase/Storage'
+
+  target 'InstagramCloneTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'InstagramCloneUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
